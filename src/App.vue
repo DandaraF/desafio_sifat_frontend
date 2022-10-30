091feadadc74d5fdcbac862e674f9fe365a80471
DandaraF <dandara-chr@hotmail.com>
2022-10-30 14:54:09 -0300

    start project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Header/>
+</template>
+
+<script>
+import './styles/global.css'
+import Header from './components/Header.vue'
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
